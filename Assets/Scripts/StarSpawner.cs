22f741cc3746fec8bbf681fa18095cfb7aed7790
Valgard G <valgard2010@gmail.com>
2026-06-06 23:54:08 +0000

    feat(space-game): added stars to space game

---
+++ Assets/Scripts/StarSpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class StarSpawner : MonoBehaviour
+{
+    void Start()
+    {
+        SpawnStars();
+    }
+
+    private void SpawnStars()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            
+        }
+    }
+}
